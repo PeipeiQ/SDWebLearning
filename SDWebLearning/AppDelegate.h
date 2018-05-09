80d79e98a3812fd12730827f661062aa7c3cbe0a
PeipeiQ <597647185@qq.com>
2018-05-09 20:34:06 +0800

    init project

---
+++ SDWebLearning/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SDWebLearning
+//
+//  Created by 沛沛 on 2018/5/9.
+//  Copyright © 2018年 沛沛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
