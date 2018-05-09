97183fea04b62d5e67f754aa03812752f0929ebb
PeipeiQ <597647185@qq.com>
2018-05-09 22:05:14 +0800

    安装pod

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '9.0'
+target 'SDWebLearning' do
+
+pod 'SDWebImage'
+pod 'AFNetworking', '~> 3.0'
+
+end
---
+++ Pods/Target Support Files/Pods-SDWebLearning/Pods-SDWebLearning-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SDWebLearning : NSObject
+@end
+@implementation PodsDummy_Pods_SDWebLearning
+@end
